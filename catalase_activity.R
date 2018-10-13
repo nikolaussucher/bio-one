7e113133e66f4f3adbcf0f3ee0d6fdcf4325e1e7
Nikolaus Sucher <nsucher@rcc.mass.edu>
2018-10-13 11:35:03 -0400

    added missing figure legend in chem_aspects

--- catalase_activity.R
+++ catalase_activity.R
@@ -14,11 +14,19 @@ data <- read_csv("data/catalase_ph.csv")
 
 data <- data %>% group_by(Group) %>% mutate(Normalized = activity/max(activity)*100)
 
+potato_juice <- tibble(Group = rep("d",3), pH = c(8.2,7.3,8.5), activity = c(13,10,14))
+
+#linear model
+lmod <- lm(pH ~ activity, data = data)
+predict(lmod, newdata = potato_juice)
 
 #plot 
 ggplot(data=data) +
   geom_point(mapping = aes(x = pH, y = activity, shape = Group, color = Group),
            size = 4, show.legend = FALSE) +
+#draw points for pH of potato juice predicted from linear model
+#  geom_point(data = potato_juice, mapping = aes(x = pH, y = activity, shape = Group, color = Group),
+#             size = 4, show.legend = FALSE) +
   geom_smooth(mapping = aes(x = pH, y = activity),
               show.legend = FALSE, size = 2) +
   scale_x_continuous(breaks = seq(0, 14, 1)) +
